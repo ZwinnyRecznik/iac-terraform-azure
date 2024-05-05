15d5b3e642234f1987325b5c5358d49b638622ac
ZwinnyRecznik <152792430+ZwinnyRecznik@users.noreply.github.com>
2024-05-05 14:53:32 +0200

    Update variables.tf

--- azure-functions/variables.tf
+++ azure-functions/variables.tf
@@ -1,21 +1,23 @@
 variable "komendadruga" {
   type    = string
-  default = ["us-west-1a"]
+  default = "us-west-1a"
 }
+
+
 
 variable "linuxfunkcjatestapki" {
   type    = string
-  default = ["us-west-1a"]
+  default = "us-west-1a"
 }
 
 variable "przyklad_service_plan" {
   type    = string
-  default = ["us-west-1a"]
+  default = "us-west-1a"
 }
 
 
 variable "przykladfunkcji" {
   type    = string
-  default = ["us-west-1a"]
+  default = "us-west-1a"
 }
 
