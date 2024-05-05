74f974ff4b9374a91912f837def5e6651d2da0f2
ZwinnyRecznik <152792430+ZwinnyRecznik@users.noreply.github.com>
2024-05-05 14:38:12 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -97,11 +97,11 @@ resource "azurerm_app_service_plan" "example" {
   }
 }
 
-resource "azurerm_function_app" "example" {
+resource "azurerm_function_app" "przykladfunkcji" {
   name                       = "test-azure-functions"
-  location                   = azurerm_resource_group.example.location
-  resource_group_name        = azurerm_resource_group.example.name
-  app_service_plan_id        = azurerm_app_service_plan.example.id
-  storage_account_name       = azurerm_storage_account.example.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  location                   = azurerm_resource_group.przykladfunkcji.location
+  resource_group_name        = azurerm_resource_group.przykladfunkcji.name
+  app_service_plan_id        = azurerm_app_service_plan.przykladfunkcji.id
+  storage_account_name       = azurerm_storage_account.przykladfunkcji.name
+  storage_account_access_key = azurerm_storage_account.przykladfunkcji.primary_access_key
 }
