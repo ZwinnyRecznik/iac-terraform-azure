7444398df6957ae32655692096eca465b47c54b3
ZwinnyRecznik <152792430+ZwinnyRecznik@users.noreply.github.com>
2024-05-05 18:22:26 +0200

    Update provider.tf

--- azure-blob-storage/provider.tf
+++ azure-blob-storage/provider.tf
@@ -1,1 +1,21 @@
+terraform {
+  backend "local" {
+    path = "./terraform.tfstate"
+  }
+}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Ten kawałek kodu zapewania nam, ze azure nie łaczy się z żadną subskrypcją
+
+provider "azurerm" {
+  skip_provider_registration = true 
+  features {}
+}
 
