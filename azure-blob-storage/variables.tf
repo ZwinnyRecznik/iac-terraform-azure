4948b795ecae67335934a6af492d7e48d8f9bf81
ZwinnyRecznik <152792430+ZwinnyRecznik@users.noreply.github.com>
2024-05-05 18:41:24 +0200

    Update variables.tf

--- azure-blob-storage/variables.tf
+++ azure-blob-storage/variables.tf
@@ -1,1 +1,34 @@
+variable "name1" {
+  type    = string
+  default = "location1.py"
+}
 
+variable "name2" {
+  type    = string
+  default = "location2.py"
+}
+
+variable "storageaccountname" {
+  type    = string
+  default = "vazurerm_resource_group.example.location.py"
+}
+
+variable "example-resources" {
+  type    = string
+  default = "location3.py"
+}
+
+variable "examplestoraccount" {
+  type    = string
+  default = "azurerm_resource_group.example.location.py"
+}
+
+variable "vhds" {
+  type    = string
+  default = "location4.py"
+}
+
+variable "example-blob-name" {
+  type    = string
+  default = "location5.py"
+}
